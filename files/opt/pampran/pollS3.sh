e0fd110bb0d1196bf82b8f23c5b42c9c087e1b0d
Mark Wallace <mark.t.wallace@gmail.com>
2016-05-16 20:33:20 -0700

    scripts to poll S3

---
+++ files/opt/pampran/pollS3.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+cd
+aws s3 --quiet cp s3://amiwork/version .
+DEPLOY_DIR=`sed -n '1p' < version`
+TARBALL=`sed -n '2p' < version`
+PACKAGE=`sed -n '3p' < version`
+if [ ! -e install ]; then
+    mkdir install
+fi
+cd install
+if [ -e ${DEPLOY_DIR} ]; then
+    # already installed this version
+    exit 0
+fi
+mkdir ${DEPLOY_DIR}
+cd ${DEPLOY_DIR}
+aws s3 cp s3://amiwork/${TARBALL} .
+npm install ${TARBALL}
+cd node_modules
+cd ${PACKAGE}
+# kill existing server before restarting
+pkill -f forever/bin/monitor
+LOGDIR=/var/log/pampran
+forever start -a -l ${LOGDIR}/forever.log -o ${LOGDIR}/out.log -e ${LOGDIR}/err.log -c "npm start" ./ &
---
+++ files/opt/pampran/repeatPoll.sh
@@ -0,0 +1,5 @@
+for ((n=0;n<30;n++))
+do
+ ./pollS3.sh
+ sleep 20
+done
