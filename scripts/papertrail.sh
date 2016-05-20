3db17aab0c6ece2e8763810b646ab8e77de8116f
Mark Wallace <mark.t.wallace@gmail.com>
2016-05-19 17:26:41 -0700

    Update papertrail to 0.17

--- scripts/papertrail.sh
+++ scripts/papertrail.sh
@@ -3,7 +3,8 @@ set -o errexit -o pipefail -o nounset
 # set -o xtrace
 
 echo Downloading papertrail remote_syslog...
-wget https://github.com/papertrail/remote_syslog2/releases/download/v0.13/remote_syslog_linux_amd64.tar.gz
+wget https://github.com/papertrail/remote_syslog2/releases/download/v0.17/remote_syslog_linux_amd64.tar.gz
+
 tar xzf remote_syslog*.tar.gz
 rm remote_syslog*.tar.gz
 
