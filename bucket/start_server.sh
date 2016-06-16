88b3c197c41dd1c4a48106a3651d061007475267
Mark Wallace <mark.t.wallace@gmail.com>
2016-06-15 18:09:19 -0700

    Change forever command to log to only one file

--- bucket/start_server.sh
+++ bucket/start_server.sh
@@ -2,9 +2,9 @@
 
 export REDIS_URL=127.0.0.1:6379
 
-LOGDIR=/var/log/papertrail/
+LOGDIR=/var/log/papertrail
 
 cd /opt/server/install/package
 
 echo Starting server with forever...
-forever start -m 5 -a -l ${LOGDIR}/forever.log -o ${LOGDIR}/out.log -e ${LOGDIR}/err.log -c "npm start" ./ &
+forever start -m 5 -l ${LOGDIR}/serv.log -a lib/main.js &
