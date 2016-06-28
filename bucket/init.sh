7416f0dda1bad3d993f076b8a69a317b00e3718c
Mark Wallace <mark.t.wallace@gmail.com>
2016-06-28 08:14:16 -0700

    cache cluster discovery

--- bucket/init.sh
+++ bucket/init.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 source /opt/env.sh
+
+aws elasticache describe-cache-clusters \
+    --cache-cluster-id ${CACHE_CLUSTER} \
+    --show-cache-node-info
+
 /opt/server/install_server.sh
 /opt/server/start_server.sh
 /opt/server/repeat_poll.sh
